24b6149e5bd96da359e6668fabfa395a1cb2a90c
basitmalik97 <basitmalik97@gmail.com>
2024-01-08 12:05:22 -0500

    signup bug fix

--- client/src/App.js
+++ client/src/App.js
@@ -48,6 +48,7 @@ function App() {
               <Switch>
                 <Route exact path="/" component={Home} />
                 <Route exact path="/login" component={Login} />
+                <Route exact path="/signup" component={Signup} />
 
               </Switch>
           </Provider>
